2bf6761091387e3faa0c2a12ae63d59475e95615
Joseph Sommer <jjs451@msstate.edu>
2017-09-28 16:27:39 -0500

    added example of what it looks like to add a paramter and how the data gets returned

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var $ = require('jquery')
+$.ajaxSetup({
+  async: false
+});
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import ApiWrapper from './ApiWrapper.js';
 
-var url = "https://api.dp.la/v2/items?sourceResource.description=%22deeply+rooted%22&api_key=304ebe4fa961241e648edf1035166735"
-ApiWrapper.makeCall(url)
+var results = ApiWrapper.makeCall("portraits--georgia")
+console.log(results)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
