c1a8597c30f1fc68a2977f7098cc6f596217e59b
Joseph Sommer <jjs451@msstate.edu>
2017-10-19 14:54:11 -0500

    moved ajax setup to ApiWrapper

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import ApiWrapper from './ApiWrapper.js';
-var $ = require('jquery')
-$.ajaxSetup({
-  async: false
-});
 
-var results = ApiWrapper.makeCall({language:"english"})
+var results = ApiWrapper.makeCall({language:"english", subject:"cooking", page:"2"})
 console.log(results)
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
