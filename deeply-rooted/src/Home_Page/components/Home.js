5998cb856b427c17849ed5202357738e07619076
henryjr1 <31903011+henryjr1@users.noreply.github.com>
2018-02-28 15:24:53 -0600

    Accessibility (#52)
    * Issue #42 Fix
    
    Fixed alternative text for images
    
    * Fix Issue #43
    
    Labels for all search boxes are fixed

--- deeply-rooted/src/Home_Page/components/Home.js
+++ deeply-rooted/src/Home_Page/components/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
         <div>
           <div className="homepage">
             <h2>Deeply Rooted</h2>
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="Deeply Rooted Logo" />
             <br />
             <br />
             <p>Knowledge. It makes the world grow. It connects people. It helps people achieve their dreams. Deeply Rooted believes in the power of knowledge.  They have a shared digital collection filled with photographs, maps, books, news footages, and other media items. They have a wide range of contributors including Auburn University Libraries, Mississippi State Libraries, University of Kentucky, University of Florida, University of Georgia, and Wake Forrest University. Their goals include bringing in new materials and partners, and connecting people through curation, education, and community.</p>
