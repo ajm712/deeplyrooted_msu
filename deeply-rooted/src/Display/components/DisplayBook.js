85207cced85b52098b53dc639c41493f2b1b0631
ajm712 <austintexas12@gmail.com>
2018-03-26 17:07:28 -0500

    Uncommented line 58 in Display.css

--- deeply-rooted/src/Display/components/DisplayBook.js
+++ deeply-rooted/src/Display/components/DisplayBook.js
@@ -56,6 +56,24 @@ class Books extends React.Component {
           <span className="inline">
             <button autoFocus id="component" name="componentView" disabled={this.props.view === "componentView"} className="toggleButtonLeft fa fa-th-large" onClick={this.displayToggle}>  Image View</button>
             <button autoFocus id="table" name="tableView" disabled={this.props.view === "tableView"} className="toggleButtonRight fa fa-table" onClick={this.displayToggle}>  Table View</button>
+            <div className="inLine">
+        <form>
+          <label>
+            <select className="dropDown" onChange={this.changeInput} defaultValue="20">
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+              <option value="50">50</option>
+              <option value="60">60</option>
+              <option value="70">70</option>
+              <option value="80">80</option>
+              <option value="90">90</option>
+              <option value = "100">100</option>
+            </select>
+          </label>
+        </form>
+      </div>
           </span>  
           <div>
             {api}
