0e93482902d6642ff1edd5d71fd4cb0461614aa3
DStampsJr <dstampsjr@gmail.com>
2018-04-23 17:33:40 -0500

    make the code pretty

--- deeply-rooted/src/Map_Page/components/Map.js
+++ deeply-rooted/src/Map_Page/components/Map.js
@@ -9,23 +9,20 @@ import Advanced from '../../Adv_Search/components/AdvancedSearch.js';
 import TextBox from '../../Adv_Search/components/TextBox.js';
 import Form from '../../Adv_Search/components/Form.js';
 
-
-
-//Create Markers and the Popups
-
-
 class CustomComponent extends React.Component {
-  
   constructor(props) {
     super(props)
     this.data = require('./states.json');
     this.api = ApiWrapper.getstateFacet().facets["sourceResource.spatial.state"].terms
 }
+
+//Function for opening State page
 state_search(event) {
   var x = event.target.name
   var results = ApiWrapper.makeCall({state: x, page_size: "30", page: "1"});
   ReactDOM.render(<Books view="componentView" results={results} pageSize= "30"/>, document.getElementById('root'));
 }
+
   //Set starting position of map
   state = {
     lat: 37.090240,
@@ -52,6 +49,7 @@ state_search(event) {
       position: MapPropTypes.latlng,
     }
     
+    //Creates markers
     const MyMarkersList = ({ markers }) => {
       const items = markers.map(({ key, ...props }) => (
         <MyPopupMarker key={key} {...props} />
@@ -61,10 +59,12 @@ state_search(event) {
     MyMarkersList.propTypes = {
       markers: PropTypes.array.isRequired,
     }
-    
+
+    //centers starting map
     const center = [this.state.lat, this.state.lng]
     const location = this.data
     var i;
+
     //create array of states in database
     var markers = [];
     for (i = 0; i < this.api.length; i++) {
@@ -74,12 +74,13 @@ state_search(event) {
         }
       }
     }
+    
+    //loops all states onto markers
     for (i = 0; i < location.states.length; i++) {
       markers.push(
         { key: location.states[i].name,state: location.states[i].name , position: [location.states[i].latitude, location.states[i].longitude], children: 'There are ' + location.states[i].count + ' books in ' + location.states[i].name + " ",  bubblingMouseEvents: true } 
 )
     }
-    //show results
     return (
       <div className="div">
         <Map id="mapid" center={center} zoom={this.state.zoom}>
