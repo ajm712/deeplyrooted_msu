bbec3581be3c171817e9fb529bad9729041daaa9
teriannaj <tdj190@msstate.edu>
2018-04-23 13:25:08 -0500

    Updated FAQs page
    Added a few questions and answers

--- deeply-rooted/src/Faq_Page/components/Questions.js
+++ deeply-rooted/src/Faq_Page/components/Questions.js
@@ -9,7 +9,17 @@ class Questions extends Component {
         <div className="questionspage">
           <h2>Frequently Asked Questions</h2>
           
-             <p>Placeholder for faqs and/or gifs.</p>
+           <p> 1. What is Random Selection?<br></br>
+            Random Selection performs a random search and then displays the results from it.<br></br>
+            </p>
+            
+            
+            <p> 2. What is the difference between image view and table view?
+              <br></br> Image view displays images from search that display infomration when you click on it. Table view displays the information in a table with no images.
+            </p>
+
+              
+            
 
           <p>For information on how to contribute to the content of Deeply Rooted please visit MSU's Deeply Rooted <a href="http://lib.msstate.edu/deeplyrooted#specs">contribution page</a>
           </p>
